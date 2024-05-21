820b4b4ff1acc5c58fa8a5bcd24f33f6d833a817
AgustinLiviñi <agustinlivini07@gmail.com>
2024-05-21 19:34:35 -0300

    changes

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery  } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:3001'
+        baseUrl: 'http://localhost:3000'
     }),
     endpoints: (builder) => ({
         getUser: builder.query({
